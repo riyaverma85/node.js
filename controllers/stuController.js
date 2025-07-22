5788ef14011ead69cf8d1daa5ee088bc936e9a58
riyaverma85 <riyaverma2976@gmail.com>
2025-07-21 22:10:44 -0700

    salatry

--- controllers/stuController.js
+++ controllers/stuController.js
@@ -1,20 +1,20 @@
 
-const homepage=(req,res)=>{
+const namepage=(req,res)=>{
     res.send("<h1>Welcome to the home page</h1>");
 }
-const aboutpage=(req,res)=>{
+const postpage=(req,res)=>{
     res.send("<h1>Welcome to the about page</h1>");
 }
-const subjectage=(req,res)=>{
+const salarypage=(req,res)=>{
     res.send("<h1>Welcome to the subject page</h1>");
 }
-const feespage=(req,res)=>{
+const citypage=(req,res)=>{
     res.send("<h1>Welcome to the fees page</h1>");
 }
 
 module.exports={
-    homepage,
-    aboutpage,
-    subjectage,
-    feespage
+    namepage,
+    postpage,
+    salarypage,
+    citypage
 }
--- route/stuRoute.js
+++ route/stuRoute.js
@@ -2,10 +2,10 @@
 const express=require("express");
 const route=express.Router();
 const stuController=require("../controllers/stuController");
-route.get("/home",stuController.homepage);
-route.get("/about",stuController.aboutpage);
-route.get("/subject",stuController.subjectage);
-route.get("/fees",stuController.feespage);
+route.get("/name",stuController.namepage);
+route.get("/post",stuController.postpage);
+route.get("/salary",stuController.salarypage);
+route.get("/city",stuController.citypage);
 
 
 
