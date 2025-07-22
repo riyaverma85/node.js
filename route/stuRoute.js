c82f474b735b223f61b612e33652013584bbc7a3
riyaverma85 <riyaverma2976@gmail.com>
2025-07-21 22:06:54 -0700

    route

--- route/stuRoute.js
+++ route/stuRoute.js
@@ -1,6 +1,12 @@
 
 const express=require("express");
 const route=express.Router();
+const stuController=require("../controllers/stuController");
+route.get("/home",stuController.homepage);
+route.get("/about",stuController.aboutpage);
+route.get("/subject",stuController.subjectage);
+route.get("/fees",stuController.feespage);
+
 
 
 module.exports=route;
