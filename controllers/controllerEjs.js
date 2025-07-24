9064e29f9ebc151aaef87c47d18692f570c0d049
riyaverma85 <riyaverma2976@gmail.com>
2025-07-24 00:40:48 -0700

    controller

--- controllers/controllerEjs.js
+++ controllers/controllerEjs.js
@@ -2,7 +2,23 @@
 const homepage=(req,res)=>{
     res.render("home");
 }
+const aboutpage=(req,res)=>{
+    res.render("home");
+}
+const servicepage=(req,res)=>{
+    res.render("home");
+}
+const gallerypage=(req,res)=>{
+    res.render("home");
+}
+const contactpage=(req,res)=>{
+    res.render("home");
+}
 
 module.exports={
-    homepage
+    homepage,
+    aboutpage,
+    servicepage,
+    gallerypage,
+    contactpage
 }
--- route/stuRouteEjs.js
+++ route/stuRouteEjs.js
@@ -3,6 +3,7 @@ const express=require("express");
 const route=express.Router();
 const controllerEjs=require("../controllers/controllerEjs");
 route.get("/",controllerEjs.homepage)
+route.get("/About",controllerEjs.homepage)
 
 
 module.exports=route;
