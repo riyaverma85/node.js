58e539acb9fa81196dd5bcd18a7d84a2b54d2254
riyaverma85 <riyaverma2976@gmail.com>
2025-07-24 00:21:43 -0700

    route

--- controllers/stuController.js
+++ controllers/stuController.js
@@ -1,1 +1,20 @@
 
+const namepage=(req,res)=>{
+    res.send("<h1>Welcome to the home page</h1>");
+}
+const postpage=(req,res)=>{
+    res.send("<h1>Welcome to the about page</h1>");
+}
+const salarypage=(req,res)=>{
+    res.send("<h1>Welcome to the subject page</h1>");
+}
+const citypage=(req,res)=>{
+    res.send("<h1>Welcome to the fees page</h1>");
+}
+
+module.exports={
+    namepage,
+    postpage,
+    salarypage,
+    citypage
+}
